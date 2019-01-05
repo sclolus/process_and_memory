325d5d282e2b42be819a30bf20ba38f6e6401d4e
Sebastien `sclolus` Clolus <sclolus@student.42.fr>
2019-01-05 01:57:56 +0000

    Ok so, I eventually got out of this madness, child_array filling is implemented and working. TODO: implement kernel stack mapping, process age, killing myself

--- sys_get_pid_info.c
+++ sys_get_pid_info.c
@@ -24,7 +24,6 @@ static int32_t  get_user_path_from_struct_path(struct path *path, char *buffer, 
 		return -1;
 	memcpy(buffer, ret, size);
 	return 0;
-
 }
 
 
@@ -32,7 +31,6 @@ static void	printk_file_path(struct file *file, char *prefix)
 {
 	static char buffer[PAGE_SIZE];
 	char	    *ptr;
-
 
 	ptr = dentry_path_raw(file->f_path.dentry, buffer, sizeof(buffer));
 	if (IS_ERR(ptr)) {
@@ -59,11 +57,22 @@ static int32_t	test_function(void *data)
 	return (0);
 }
 
+uint64_t    klist_len(const struct list_head *list) {
+	uint64_t	     __len = 0;
+	struct list_head    *pos = NULL;
+
+	list_for_each(pos, list) {
+		__len++;
+	}
+	return __len;
+}
+
 SYSCALL_DEFINE2(get_pid_info, struct pid_info __user *, to, int, pid)
 {
 	struct pid_info	    *info;
 	struct task_struct  *task;
 	int64_t		    ret;
+	uint64_t	    required_child_array_size;
 
 	printk(KERN_INFO LOG "get_pid_info() was called by pid: %d\n", current->pid);
 
@@ -72,28 +81,57 @@ SYSCALL_DEFINE2(get_pid_info, struct pid_info __user *, to, int, pid)
 		goto err;
 	}
 
+	if (0 != copy_from_user(info, to, sizeof(struct pid_info))) {
+		kfree(info);
+		ret = -EPERM;
+		goto err;
+	}
+
 	rcu_read_lock();
 	if (NULL == (task = find_task_by_pid_ns(pid, task_active_pid_ns(current)))) {
 		ret = -ESRCH;
 		goto err;
 	}
-	rcu_read_unlock();
+	ret = 0;
+ 	rcu_read_unlock();
 	task_lock(task);
 
-	info->pid = task->pid;
+	info->pid = task_tgid_vnr(task);
 	info->state = task->state;
 	info->stack = task->stack;
 	info->age = /* task->start_time */ task->cputime_expires.sum_exec_runtime;
 	info->parent_pid = task->real_parent->pid;
 	ret = get_user_path_from_struct_path(&task->fs->root, info->root_path, sizeof(info->root_path));
 	ret |= get_user_path_from_struct_path(&task->fs->pwd, info->cwd, sizeof(info->cwd));
-
+	required_child_array_size = klist_len(&task->children) * sizeof(pid_t);
 	if (ret != 0) {
 		kfree(info);
 		ret = -EPERM;
 		goto err_tlock_held;
 	}
 
+	if (required_child_array_size > info->child_array_size) {
+		info->child_array_size = required_child_array_size;
+		info->syscall_status = ERR_CHILD_ARRAY_TOO_SMALL;
+		ret = -ENOMEM;
+		printk(KERN_INFO LOG "child_array was too small\n");
+	} else {
+		struct task_struct *pos = NULL;
+		uint64_t	    i = 0;
+		pid_t		    vpid;
+
+		printk(KERN_INFO LOG "Entering list_for_each_entry\n");
+		list_for_each_entry(pos, &task->children, sibling) {
+			vpid = task_tgid_vnr(pos);
+			printk(KERN_INFO LOG "assigning pid entry for childen : %llu -> %u, pos->tgid: %u\n", i, vpid, pos->tgid);
+			copy_to_user(&info->child_array[i], &vpid, sizeof(pid_t)); // check return value
+			i++;
+		}
+		printk(KERN_INFO LOG "Exiting list_for_each_entry\n");
+		info->child_array_size = required_child_array_size;
+	}
+
+	info->syscall_status = SUCCESS;
 	if (0 != copy_to_user(to, info, sizeof(struct pid_info))) {
 		kfree(info);
 		ret = -EPERM;
@@ -101,9 +139,9 @@ SYSCALL_DEFINE2(get_pid_info, struct pid_info __user *, to, int, pid)
 	}
 
 	kfree(info);
-	test_function(NULL);
-	task_unlock(task);
-	return 0;
+	/* test_function(NULL); */
+	task_unlock(task); //should need this though...
+	return ret;
 err_tlock_held:
 	task_unlock(task);
 err:
--- test.c
+++ test.c
@@ -5,33 +5,58 @@
 #include <limits.h>
 #include <dirent.h>
 #include <fcntl.h>
+#include <assert.h>
 
 
 /* #define __NR_get_pid_info 293 */
 #define __NR_get_pid_info 335
 
-struct pid_info {
-	pid_t	    pid;
-	long	    state;
-	void	    *stack;
-	uint64_t    age; //Not sure about this
-	pid_t	    *child_array; //where is max child ?
-	pid_t	    parent_pid;
-	char	    root_path[PATH_MAX];
-	char	    cwd[PATH_MAX];
+
+/* Further describe the reason of error if the sys_get_pid_info fails */
+enum	get_pid_info_status {
+	SUCCESS,
+	ERR_CHILD_ARRAY_TOO_SMALL,
+	ERR_UNKNOWN,
 };
+
+struct pid_info {
+	pid_t			     pid;
+	long			     state;
+	void			    *stack;
+	uint64_t		     age;   //Not sure about this
+	pid_t			    *child_array;   //where is max child ?
+	size_t			     child_array_size;
+	enum get_pid_info_status     syscall_status;
+	pid_t			     parent_pid;
+	char			     root_path[PATH_MAX];
+	char			     cwd[PATH_MAX];
+};
+
+static void print_children_info(struct pid_info *info)
+{
+	uint64_t    i = 0;
+
+	assert(info->syscall_status == SUCCESS);
+	assert(sizeof(pid_t) == 4);
+	while (i < info->child_array_size / sizeof(pid_t)) {
+		printf("Pid for child: %lu -> %u\n", i, info->child_array[i]);
+		i++;
+	}
+}
 
 static void print_pid_info(struct pid_info *info)
 {
-	printf("pid: %d\nstate:%ld\nstack_ptr: %p\nage: %lu, child_array: %p, parent_pid: %d\nroot_path: %s\ncwd: %s\n",
+	printf("pid: %d\nstate:%ld\nstack_ptr: %p\nage: %lu, child_array: %p, no_children = %lu, parent_pid: %d\nroot_path: %s\ncwd: %s\n",
 		info->pid,
 		info->state,
 		info->stack,
 		info->age,
 		info->child_array,
+		info->child_array_size / sizeof(pid_t),
 		info->parent_pid,
 		info->root_path,
 		info->cwd);
+	print_children_info(info);
 }
 
 static int32_t test_function(void)
@@ -55,23 +80,56 @@ static int32_t test_function(void)
 }
 
 
+static int32_t	wrapper_get_pid_info(struct pid_info *info, pid_t pid) {
+	uint64_t    current_array_size = 32;
+
+	do {
+		if (NULL == (info->child_array = malloc(sizeof(pid_t) * current_array_size))) {
+			perror(NULL);
+			exit(EXIT_FAILURE);
+		}
+		info->child_array_size = current_array_size * sizeof(pid_t);
+		current_array_size *= 2;
+		printf("Calling get_pid_info with child_array of size: %lu\n", info->child_array_size);
+		if (-1 == syscall(__NR_get_pid_info, info, pid))
+		{
+			if (info->syscall_status == ERR_CHILD_ARRAY_TOO_SMALL) {
+				printf("get_pid_info informed that the child_array was too small for size: %lu\n", current_array_size);
+				current_array_size *= 2;
+				free(info->child_array);
+				continue ;
+			}
+			printf("get_pid_info returned -1\n");
+			free(info->child_array);
+			return (-1);
+		}
+
+		return (0);
+	} while (info->syscall_status != SUCCESS);
+	/// ???
+	return (0);
+}
+
 
 int main(void)
 {
 	struct pid_info	info;
+	uint32_t    i = 0;
 
-
-	sleep(1);
-		test_function() == 0 ? printf("Test_function was successfull\n")
-		: printf("Test_function was successfull\n");
-
-	if (-1 == syscall(__NR_get_pid_info, &info, getpid()))
-	{
-		printf("get_pid_info returned -1\n");
-		return EXIT_FAILURE;
+	while (i < 5) {
+		if (0 == fork()) {
+			assert(sleep(2) == 0);
+			exit(0);
+		}
+		i++;
 	}
 
+	(void)test_function;
+	/* sleep(1); */
+	/* test_function() == 0 ? printf("Test_function was successfull\n") */
+	/* 	: printf("Test_function was successfull\n"); */
+	if (-1 == wrapper_get_pid_info(&info, 1))
+		exit(EXIT_FAILURE);
 	print_pid_info(&info);
-
 	return 0;
 }
